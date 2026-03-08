4e8ebfb8f9b77b5b5685d08c36b544f0c559a533
neonDebugger <shivamkulat14@gmail.com>
2026-03-08 17:59:16 +0530

    new program

--- while_loop.py
+++ while_loop.py
@@ -1,6 +1,20 @@
 
-i = int(input("Enter the number : "))
-while i<= 10:
-    i += 2
-    print(f"{i}")
+# i = int(input("Enter the number : "))
+# while i<= 10:
+#     i += 2
+#     print(f"{i}")
 
+
+num = int(input("Enter number : "))
+temp= num 
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 +digit
+    num = num // 10
+
+if temp == reverse :
+    print("palindrome Number ")
+else :
+    print("not Palindrome")
